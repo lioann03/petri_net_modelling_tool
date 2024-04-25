c5446184e6f1dd518af73cab06ebb449bea94ffc
Loukia Christina Ioannou <lioann03@ucy.ac.cy>
2024-04-25 10:21:34 +0300

    Clingo Code and Sort Answers Code

---
+++ sort_answers.py
@@ -0,0 +1,11 @@
+clingo version 5.4.0
+Reading from RandomPetriNets/randomPN.lp ...
+Solving...
+Answer: 1
+placeholds(p0,a0,0) placeholds(p0,a1,0) placeholds(p0,b0,0) placeholds(p0,c1,0) placeholds(p0,b1,0) placeholds(p0,c3,0) placeholds(p4,b2,0) placeholds(p8,c5,0) placeholds(p8,a5,0) placeholds(p8,a6,0) placeholds(p8,c6,0) placeholds(p8,c7,0) placeholds(p8,c9,0) placeholdsbond(p0,a2,c0,0) placeholdsbond(p0,a3,c2,0) placeholdsbond(p0,a8,c10,0) placeholdsbond(p2,a4,c4,0) placeholdsbond(p2,a7,c8,0)
+SATISFIABLE
+
+Models       : 1
+Calls        : 1
+Time         : 0.003s (Solving: 0.00s 1st Model: 0.00s Unsat: 0.00s)
+CPU Time     : 0.003s
